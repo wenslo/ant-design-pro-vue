a4c6df37d5c2643dfeb9a23ff475a5297ce33fe5
wenhailin <564544234@qq.com>
2019-11-10 19:58:22 +0800

    some change to run it with spring-boot-demo

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,7 +8,17 @@ import { ACCESS_TOKEN } from '@/store/mutation-types'
 // 创建 axios 实例
 const service = axios.create({
   baseURL: process.env.VUE_APP_API_BASE_URL, // api base_url
-  timeout: 6000 // 请求超时时间
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8'
+  },
+  // #transformRequest 允许在向服务器发送前，修改传递参数
+  // #这里通过Qs.stringify转换为表单查询参数
+  // transformRequest: [data => {
+  // 	return Qs.stringify(data)
+  // }],
+  // 设置允许跨域
+  withCredentials: true,
+  timeout: 80000 // 超时时间设置
 })
 
 const err = (error) => {
@@ -49,6 +59,7 @@ service.interceptors.request.use(config => {
 
 // response interceptor
 service.interceptors.response.use((response) => {
+  console.log(response.data)
   return response.data
 }, err)
 
