2c6672c988d0f61dc62de963f68cd1656ffce374
wenhailin <564544234@qq.com>
2020-01-07 18:40:15 +0800

    test

--- src/views/user2/list.vue
+++ src/views/user2/list.vue
@@ -31,37 +31,7 @@
       :columns="columns"
       :data="loadData"
     >
-
-      <span slot="action" slot-scope="text, record">
-        <a @click="handleEdit(record)">编辑</a>
-        <a-divider type="vertical" />
-        <a-dropdown>
-          <a class="ant-dropdown-link">
-            更多 <a-icon type="down" />
-          </a>
-          <a-menu slot="overlay">
-            <a-menu-item>
-              <a href="javascript:;">详情</a>
-            </a-menu-item>
-            <a-menu-item>
-              <a href="javascript:;">禁用</a>
-            </a-menu-item>
-            <a-menu-item>
-              <a href="javascript:;">删除</a>
-            </a-menu-item>
-          </a-menu>
-        </a-dropdown>
-      </span>
     </s-table>
-
-    <a-modal
-      title="操作"
-      style="top: 20px;"
-      :width="800"
-      v-model="visible"
-      @ok="handleOk"
-    >
-    </a-modal>
 
   </a-card>
 </template>
@@ -77,42 +47,36 @@ export default {
   },
   data () {
     return {
-      description: '列表使用场景：后台管理中的权限管理以及角色管理，可用于基于 RBAC 设计的角色权限控制，颗粒度细到每一个操作类型。',
-
-      visible: false,
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 5 }
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 }
-      },
-      form: null,
-      mdl: {},
-
-      // 高级搜索 展开/关闭
-      advanced: false,
-      // 查询参数
       queryParam: {},
       // 表头
       columns: [
         {
           title: '唯一识别码',
-          dataIndex: 'id'
+          dataIndex: 'id',
+          key: 'id'
         },
         {
           title: '用户名',
-          dataIndex: 'username'
-        }, {
+          dataIndex: 'username',
+          key: 'username'
+        },
+        {
+          title: '昵称',
+          dataIndex: 'nickname',
+          key: 'nickname'
+        },
+        {
+          title: '启用状态',
+          dataIndex: 'enabled',
+          key: 'enabled'
+        },
+        {
           title: '操作',
           width: '150px',
           dataIndex: 'action',
           scopedSlots: { customRender: 'action' }
         }
       ],
-      selectedRowKeys: [],
-      selectedRows: [],
       // 加载数据方法 必须为 Promise 对象
       loadData: parameter => {
         return getUserList(parameter)
@@ -126,20 +90,6 @@ export default {
   created () {
   },
   methods: {
-    handleEdit (record) {
-      this.mdl = Object.assign({}, record)
-
-      this.mdl.permissions.forEach(permission => {
-        permission.actionsOptions = permission.actionEntitySet.map(action => {
-          return { label: action.describe, value: action.action, defaultCheck: action.defaultCheck }
-        })
-      })
-
-      this.visible = true
-    },
-    handleOk () {
-
-    },
     onChange (selectedRowKeys, selectedRows) {
       this.selectedRowKeys = selectedRowKeys
       this.selectedRows = selectedRows
